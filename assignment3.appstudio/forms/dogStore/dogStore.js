4eabe2d5b92baf040e6140444ec4464ec95da7f4
Henry Jiang <hjiang2000@gmail.com>
2021-03-14 18:05:16 -0500

    v4

--- assignment3.appstudio/forms/dogStore/dogStore.js
+++ assignment3.appstudio/forms/dogStore/dogStore.js
@@ -1,5 +1,4 @@
-/
-Create a nice looking one-page app that uses controls for ALL input and output. Adjust properties of the form and the controls to make it a nice color, lay things out nicely, etc. 
+/*Create a nice looking one-page app that uses controls for ALL input and output. Adjust properties of the form and the controls to make it a nice color, lay things out nicely, etc. 
 The user must be able to see a list of all the products the store carries.
 The user must be able to enter a product they want to add to their wish list. 
 The program saves everything they choose to their wish list. 
@@ -16,3 +15,10 @@ let cvBowlPrice="$32.00"
 let rhDressPrice="$27.00"
 let vhBowPrice="$12.00"
 let wLeashPrice="$27.00"
+let wishlist=[]
+
+//make this show all the wishlist items
+addWishlist.onclick=function(){
+  console.log(wishlist)
+  addWishlist.style.color:"#33FFF0"
+}
