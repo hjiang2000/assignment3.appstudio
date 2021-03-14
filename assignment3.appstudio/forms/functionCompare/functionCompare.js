bf2fc91bcbb5e294f5bbbf6f52fae37bcf678c6f
Henry Jiang <hjiang2000@gmail.com>
2021-03-14 18:45:01 -0500

    v6

--- assignment3.appstudio/forms/functionCompare/functionCompare.js
+++ assignment3.appstudio/forms/functionCompare/functionCompare.js
@@ -1,13 +1,12 @@
 
-let password1=input("please enter your password.")
-let password2=input("Please enter your password again.")
+let password1=prompt("please enter your password.")
+let password2=prompt("Please enter your password again.")
 
 function passwordMatch(password1, password2)
-{
-  if (password1==password2) {
-  console.log("The passwords are the same.")
-  }
-  else {console.log("The passwords are not the same. ")
+{if (password1==password2)
+{console.log("The passwords are the same.")}
+else {console.log("The passwords are not the same. ")
   }
 }
+
 
